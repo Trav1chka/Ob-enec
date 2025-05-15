1cc260268453fddcdc08d552ceb460999ad3d73a
Trav1chka <126341971+Trav1chka@users.noreply.github.com>
2025-05-15 09:00:17 +0200

    v1.0.1
    změna názvu proměných

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-promenna = new Date(); // vytvoří proměnnou obsahující aktuální datum
-mesic = promenna.getMonth() + 1;//leden je 0
-denVMesici = promenna.getDate();
+datum = new Date(); // vytvoří proměnnou obsahující aktuální datum
+mesic = datum.getMonth() + 1;//leden je 0
+denVMesici = datum.getDate();
 document.getElementById("datum").innerHTML = "Dnes je " + denVMesici + ". " + mesic + "."
