243c12f620636b0def2df1d2bed56e7555b09379
Trav1chka <126341971+Trav1chka@users.noreply.github.com>
2025-05-15 20:20:19 +0200

    JS v1.1.1 play() + komentare

--- script.js
+++ script.js
@@ -2,11 +2,17 @@ datum = new Date(); // vytvoří proměnnou obsahující aktuální datum
 mesic = datum.getMonth() + 1; //leden je 0
 denVMesici = datum.getDate();
 document.getElementById("datum").innerHTML = "Dnes je " + denVMesici + ". " + mesic + ".";
+
+let slova;
+let slovo;
+let pocetPismen;
 function play() {
   slova = ["net", "idi", "nahui"]; // seznam slov, ktere bude uzivatel odhadovat
   slovo = slova[Math.floor(Math.random() * length(slova) - 1) + 1]; // vyber nahodneho slova
   pocetPismen = length(slovo);
   for (let i = 0; i < pocetPismen; i++) {
+    const divSlovo = document.getElementById("pismena");
+    const div = document.createElement("div"); // vytvareni divu pro pismeno a line
     const pismeno = document.createElement("p"); // vytvareni odstavce pro pismeno
     pismeno.id = "pismeno" + String(i + 1);
     pismeno.innerHTML = slovo[i];
@@ -15,5 +21,8 @@ function play() {
     line.id = "line" + String(i + 1);
     line.src = "./graphics/temp/temp-guess_200x200.png";
     line.alt = "line" + String(i + 1);
+    div.appendChild(pismeno); 
+    div.appendChild(line);
+    divSlovo.appendChild(div); // pridani pismena a line do divu se vsema pismeny
   }
 }
