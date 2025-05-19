08b96ea86f18fecf7412c0714e2216bbbc3a359b
Trav1chka <126341971+Trav1chka@users.noreply.github.com>
2025-05-19 21:44:42 +0200

    JS - v1.2.1 / mala zmena v funkce mode()

--- script.js
+++ script.js
@@ -7,16 +7,17 @@ denVMesici = datum.getDate();
 document.getElementById("datum").innerHTML = "Dnes je " + denVMesici + ". " + mesic + ".";
 
 //* Script pro měnění režimů
+let dark_mode;
 function mode() {
-  const body = document.body;
-  body.classList.toggle('dark-mode');
   const modeImg = document.querySelector('#mode img');
-  if (body.classList.contains('dark-mode')) {
+  if (dark_mode == true)) {
       modeImg.src = './graphics/icons/darkmode-switch.png';
       modeImg.alt = 'Přepnutí na světlý režim';
+      dark_mode = false
   } else {
       modeImg.src = './graphics/icons/lightmode-switch.png';
       modeImg.alt = 'Přepnutí na tmavý režim';
+      dark_mode = true
   }
 }
 
